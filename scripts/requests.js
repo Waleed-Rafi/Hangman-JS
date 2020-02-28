0b16bf353294b1f92d2561fe96a37776f6866eae
Waleed-Rafi <waleed.rafi626@gmail.com>
2020-02-28 12:53:09 +0500

    first commit

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,21 +1,9 @@
 const getPuzzle = async (wordCount) => {
-    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    const response = await fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
     if (response.status === 200) {
         const data = await response.json()
         return data.puzzle
     } else {
         throw new Error('Unable to fetch the puzzle')
     }
-}
-
-const getLocation = () => {
-    return fetch('http://ipinfo.io/?token=b9840e9429acdb').then((response) => {
-        if (response.status === 200) {
-            return response.json()
-        } else {
-            throw new Error('Unable to fetch data')
-        }
-    }).then((data) => {
-        return `your city: ${data.city} your region: ${data.region} your country: ${data.country}`
-    })
 }
